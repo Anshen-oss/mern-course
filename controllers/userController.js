c544cf59efeb51170cbb30232fda429f28f77bc3
Anshen <plouisin@gmail.com>
2023-11-09 20:37:13 +0100

    fix update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 // Typical setup: we're using that cookie to send the token to the
 // front end and then front end right away sends back
@@ -20,9 +20,11 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
+
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
